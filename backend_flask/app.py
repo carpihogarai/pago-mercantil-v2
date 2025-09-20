c36d740b723a6edd75477e4a4061594237867b9b
carpihogarai <carpihogar.ai@gmail.com>
2025-09-20 09:39:12 -0700

    fix: add www.trends172.com to allowed origins

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -55,6 +55,7 @@ app = Flask(__name__)
 origins = [
     "https://pago-mercantil-v2-xi.vercel.app",
     "https://trends172.com",
+    "https://www.trends172.com",
     "http://localhost:3000" # Para desarrollo local
 ]
 CORS(app, origins=origins, methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type"], supports_credentials=True)
